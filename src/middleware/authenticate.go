da55d0ccd7b58bd7d2e3d1421f8f0bce02d25cda
Bean-jun <1342104001@qq.com>
2022-11-06 13:21:20 +0800

    简化代码

--- src/middleware/authenticate.go
+++ src/middleware/authenticate.go
@@ -14,27 +14,27 @@ func Auth() func(*gin.Context) {
 		if token == "" {
 			u := &entity.UserEntity{}
 			c.Set("user", u)
-		} else {
-			// 网站用户
-			parseToken, err := utils.ParseToken(token)
-			// token 过期||异常
-			if err != nil {
-				utils.FalseResponse(c, "鉴权失败")
-				c.Abort()
-				return
-			}
-
-			uid := parseToken["id"]
-			userService := services.ImplUser(&services.UserService{})
-			u := userService.FindByUserID(int(uid.(float64)))
-			// 用户已不存在
-			if u == nil {
-				utils.FalseResponse(c, "鉴权失败")
-				c.Abort()
-				return
-			}
-			c.Set("user", u)
+			return
 		}
+		// 网站用户
+		parseToken, err := utils.ParseToken(token)
+		// token 过期||异常
+		if err != nil {
+			utils.FalseResponse(c, "鉴权失败")
+			c.Abort()
+			return
+		}
+
+		uid := parseToken["id"]
+		userService := services.ImplUser(&services.UserService{})
+		u := userService.FindByUserID(int(uid.(float64)))
+		// 用户已不存在
+		if u == nil {
+			utils.FalseResponse(c, "鉴权失败")
+			c.Abort()
+			return
+		}
+		c.Set("user", u)
 
 		c.Next()
 	}
